189a6f8094a76673f43595d8e4d765e862e42c23
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-04-25 13:18:48 +0200

    atlas settings

--- src/main/java/com/kattspa/kattspa/controllers/BookingsController.java
+++ src/main/java/com/kattspa/kattspa/controllers/BookingsController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+// @CrossOrigin(origins = "http://localhost:5173")
 public class BookingsController {
 
 	private BookingService bookingService;
